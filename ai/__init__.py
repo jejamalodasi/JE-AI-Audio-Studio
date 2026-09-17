ad5a8fb1433b3e94f9366ad68388aa8a40919c40
JE Jamal Odasi <jamalhossenh45@gmail.com>
2026-09-18 00:18:09 +0600

    feat: export neural pitch timing backend

--- ai/__init__.py
+++ ai/__init__.py
@@ -2,12 +2,22 @@
 
 from .basic_pitch_transcriber import transcribe_with_basic_pitch
 from .neural_vocal_enhancement import enhance_vocal_neural
+from .pitch_timing_ai import (
+    PitchTimingAIConfig,
+    analyze_pitch_neural,
+    correct_pitch_timing_ai,
+    correct_pitch_timing_file,
+)
 from .vocal_to_melody import vocal_to_melody
 from .vocal_to_music import VocalMusicConfig, generate_from_vocal
 
 __all__ = [
     "transcribe_with_basic_pitch",
     "enhance_vocal_neural",
+    "PitchTimingAIConfig",
+    "analyze_pitch_neural",
+    "correct_pitch_timing_ai",
+    "correct_pitch_timing_file",
     "vocal_to_melody",
     "VocalMusicConfig",
     "generate_from_vocal",
